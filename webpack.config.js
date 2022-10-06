480e988e16f48e6d96a917296431b111ef23855e
fekez <fekzoletetan@gmail.com>
2022-10-06 22:13:18 +0200

    feat(login)

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('othello', './assets/othello.js')
+    .addEntry('login', './assets/styles/login.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
